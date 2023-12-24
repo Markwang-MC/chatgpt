e519d5a54e9bf7d21a05f20f105923d296c9aab9
wangfei20 <wangfei20@its.me>
2023-12-24 20:29:33 +0800

    update

--- app/page.js
+++ app/page.js
@@ -2,6 +2,11 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-  <div className="bg-white fixed inset-0 flex place-content-center place-items-center">Meet Mark!</div>
+    <div className="bg-white">
+      <div className="w-full h-[50px] bg-amber-300 flex">
+        <span>占位符</span>
+        <span>占位符</span>
+      </div>
+    </div>
   )
 }
