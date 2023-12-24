03827f44b0a016b78f78e61a8e4dadc17122687d
wangfei20 <wangfei20@its.me>
2023-12-24 21:42:10 +0800

    update

--- app/page.js
+++ app/page.js
@@ -3,11 +3,15 @@
 export default function Home() {
   return (
     <div className="bg-white">
-      <div className="w-full h-[75px] bg-amber-300 flex items-center justify-end">
+      <div className="w-full h-[75px] bg-amber-300 flex items-center justify-end rounded">
         <div className="mr-[50px]">
           <span className="mr-[50px]">占位符</span>
           <span>占位符</span>
         </div>
+      </div>
+
+      <div className="bg-neutral-300 w-full h-[500px]">
+
       </div>
     </div>
   )
