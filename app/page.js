71a7d38885746be57fcf41153f043c7de803b861
wangfei20 <wangfei20@its.me>
2024-01-03 09:57:43 +0800

    update

--- app/page.js
+++ app/page.js
@@ -4,16 +4,7 @@ export default function Home() {
   const [about, setAbout] = useState(false);
   return (
     <div className="bg-white">
-      <div className="w-full h-[75px] bg-amber-300 flex items-center justify-end rounded">
-        <div className="mr-[50px]">
-          <span className="mr-[50px]" onClick={setAbout(!about)}>about</span>
-          <span>占位符</span>
-        </div>
-      </div>
-
-      <div className="bg-neutral-300 w-full h-[500px] mx-3 mt-1 rounded">
-
-      </div>
+      
     </div>
   )
 }
