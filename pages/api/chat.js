2234b3f04c79348422f5d8e6e8c837070672bba3
wangfei20 <wangfei20@its.me>
2024-01-16 21:56:15 +0800

    update

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -3,7 +3,7 @@ export default function handler(req, res) {
   if (req.method=="POST") {
     console.log('req.body');
   }
-
+  var _text;
   const generateText = async (prompt) => {
     const response = await openai.Completion.create({
       engine: 'text-davinci-003',
@@ -15,11 +15,12 @@ export default function handler(req, res) {
 
   generateText('Once upon a time').then(text => {
     console.log({text});
+    _text=text
   }).catch(error => {
     console.error('Error:', error);
   });
 
 
-  res.status(200).json({x:1})
+  res.status(200).json({x:1,text:_text})
   // res.end("")
 }
