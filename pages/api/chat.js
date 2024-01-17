c9f71ad64f0884f31495c633cfc767642900e318
wangfei20 <wangfei20@its.me>
2024-01-17 15:10:24 +0800

    update

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -5,20 +5,14 @@ export default function handler(req, res) {
   }
 
   const openai = new OpenAI({
-    apiKey: process.env['OPENAI_API_KEY'], // This is the default and can be omitted
+    apiKey: process.env['OPENAI_API_KEY']
   });
 
-  async function main() {
-    const chatCompletion = await openai.chat.completions.create({
-      messages: [{ role: 'user', content: 'Say this is a test' }],
-      model: 'gpt-3.5-turbo',
-    });
-  }
-
-  main();
-  // print('ChatGPT Reply:', reply)
-
-
+  const chatCompletion = await openai.chat.completions.create({
+    messages: [{ role: 'user', content: 'Say this is a test' }],
+    model: 'gpt-3.5-turbo',
+    stream: false
+  });
+  console.log(chatCompletion);
   res.status(200).json({s:1})
-  // res.end("")
 }
